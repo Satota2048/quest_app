d2e65cf560de87ddb8f4e7363a76f8dc6a423bc0
Satota2048 <windowformywork@gmail.com>
2021-05-02 10:58:24 +0900

    画像プレビュー１

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageView = document.getElementById('image-view');
+  
   document.getElementById('message_image').addEventListener('change', function(e){
     console.log(123);
   });
 });
+
+// document.addEventListener('DOMContentLoaded', function(){
+//   console.log(123);
+// });
