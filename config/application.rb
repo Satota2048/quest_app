8fc8b5fc2ae78f84927c4a420190a4002dd5fd65
Satota2048 <windowformywork@gmail.com>
2021-05-12 16:31:34 +0900

    日本時間表示実験

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module QuestApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # 東京時間追加
+    config.time_zone = 'Tokyo'
   end
 end
